9a8a47a4fbd306ac8c2866d4d77ee6f84fc30d2f
Joao Samuel Lopes <jsnl85@gmail.com>
2017-06-28 13:28:21 +1000

    *Update: minification of the index.html file, to promote main header first *Update: sections of the page loaded assynchronously, based on page IDs *Update: SEO json+ld sections loaded assynchronously, *Update: page content now using microdata +Add: htaccess file added to configure HTTP caching and default pages

--- js/creative.js
+++ js/creative.js
@@ -2,98 +2,144 @@
     "use strict"; // Start of use strict
 
     if (typeof(jQuery)=='undefined' || !jQuery.fn) {if(window.console&&window.console.log)window.console.log('jQuery is not included! Skipping template init!');return;}
+    var getJson = function(url, callback){$.ajax({type:"GET",url:url,dataType:"json",cache:true,async:true,success:callback});}
+    var getHtml = function(url, callback){$.ajax({type:"GET",url:url,dataType:"html",cache:true,async:true,success:callback});}
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    if (jQuery.fn.scrollspy) {
-        $('body').scrollspy({
-            target: '.navbar-fixed-top',
-            offset: 51
-        });
-    }
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    if (jQuery.fn.affix) {
-        $('#mainNav').affix({
-            offset: {
-                top: 100
+    // Initialise the MainNav section
+    var $mainNav = $("#mainNav");
+    if ($mainNav.length > 0) {
+        getHtml("sections/mainNav.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#mainNav").html();
+            $mainNav.html(html);
+            // 
+            // Highlight the top nav as scrolling occurs
+            if (jQuery.fn.scrollspy) {
+                $('body').scrollspy({
+                    target: '.navbar-fixed-top',
+                    offset: 51
+                });
+            }
+            // Closes the Responsive Menu on Menu Item Click
+            $('.navbar-collapse ul li a').click(function() {
+                $('.navbar-toggle:visible').click();
+            });
+            // Offset for Main Navigation
+            if (jQuery.fn.affix) {
+                $('#mainNav').affix({
+                    offset: {
+                        top: 100
+                    }
+                });
             }
         });
     }
 
-    // Initialize and Configure Scroll Reveal Animation
-    if (typeof(ScrollReveal)!='undefined') {
-        window.sr = ScrollReveal();
-        sr.reveal('.sr-icons', {
-            duration: 600,
-            scale: 0.3,
-            distance: '0px'
-        }, 200);
-        sr.reveal('.sr-button', {
-            duration: 1000,
-            delay: 200
-        });
-        sr.reveal('.sr-contact', {
-            duration: 600,
-            scale: 0.3,
-            distance: '0px'
-        }, 300);
-    }
-
-    // Initialise the Testimonials carousel
-    if (jQuery.fn.owlCarousel) {
-            $(".testimonials-carousel, .mockup-carousel").owlCarousel({
-            items: 1,
-            //singleItem: true,
-            autoWidth: false,
-            autoHeight: false,
-            responsive: true,
-            loop: true,
-            addClassActive: true,
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            autoPlay: true,
-            stopOnHover: true,
-            pagination: false,
-            navigation: true,
-            lazyLoad: true,
-            navigationText: [
-                "<i class='fa fa-angle-left'></i>",
-                "<i class='fa fa-angle-right'></i>"
-            ],
-            transitionStyle: "backSlide"
+    // Initialise the About section
+    var $about = $("#about");
+    if ($about.length > 0) {
+        getHtml("sections/about.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#about").html();
+            $about.html(html);
         });
     }
 
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    if (jQuery.fn.magnificPopup) {
-        $('.popup-gallery').magnificPopup({
-            delegate: 'a',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    // Initialise the Services section
+    var $services = $("#services");
+    if ($services.length > 0) {
+        getHtml("sections/services.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#services").html();
+            $services.html(html);
+        });
+    }
+
+    // Initialise the Download section
+    var $download = $("#download");
+    if ($download.length > 0) {
+        getHtml("sections/download.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#download").html();
+            $download.html(html);
+        });
+    }
+
+    // Initialise the Testimonials section
+    var $recommendations = $("#recommendations");
+    if ($recommendations.length > 0) {
+        getHtml("sections/testimonials.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#recommendations").html();
+            $recommendations.html(html);
+            // 
+            // Initialise the Testimonials carousel
+            if (jQuery.fn.owlCarousel) {
+                $recommendations.find(".testimonials-carousel, .mockup-carousel").owlCarousel({
+                    items: 1,
+                    //singleItem: true,
+                    autoWidth: false,
+                    autoHeight: false,
+                    responsive: true,
+                    loop: true,
+                    addClassActive: true,
+                    slideSpeed: 300,
+                    paginationSpeed: 400,
+                    autoPlay: true,
+                    stopOnHover: true,
+                    pagination: false,
+                    navigation: true,
+                    lazyLoad: true,
+                    navigationText: [
+                        "<i class='fa fa-angle-left'></i>",
+                        "<i class='fa fa-angle-right'></i>"
+                    ],
+                    transitionStyle: "backSlide"
+                });
             }
         });
     }
+
+    // Initialise the Contacts section
+    var $contacts = $("#contacts");
+    if ($contacts.length > 0) {
+        getHtml("sections/contacts.min.html?v3", function(data) {
+            var $el = $('<div/>').html(data), html = $el.find("#contacts").html();
+            $contacts.html(html);
+        });
+    }
+
+    // On Window Load
+    $(window).load(function() {
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $(document).on('click', 'a.page-scroll', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        });
+
+        // Initialize and Configure Scroll Reveal Animation
+        if (typeof(ScrollReveal)!='undefined') {
+            window.sr = ScrollReveal();
+            sr.reveal('.sr-icons', { duration: 600, scale: 0.3, distance: '0px' }, 200);
+            sr.reveal('.sr-button', { duration: 1000, delay: 200 });
+            sr.reveal('.sr-contact', { duration: 600, scale: 0.3, distance: '0px' }, 300);
+        }
+
+        // Initialize and Configure Magnific Popup Lightbox Plugin
+        if (jQuery.fn.magnificPopup) {
+            $('.popup-gallery').magnificPopup({
+                delegate: 'a',
+                type: 'image',
+                tLoading: 'Loading image #%curr%...',
+                mainClass: 'mfp-img-mobile',
+                gallery: {
+                    enabled: true,
+                    navigateByImgClick: true,
+                    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+                },
+                image: {
+                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+                }
+            });
+        }
+    });
 
 })(jQuery); // End of use strict
