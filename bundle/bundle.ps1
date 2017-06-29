4ac650f07908e17e9804643f4e0d22dd14352592
Joao Samuel Lopes <jsnl85@gmail.com>
2017-06-29 13:29:53 +1000

    *Update: re-included the Pace.js progress indicator

---
+++ bundle/bundle.ps1
@@ -0,0 +1,2 @@
+Get-Content -Encoding:UTF8 ../vendor/jquery/jquery.min.js, ../vendor/bootstrap/js/bootstrap.min.js, ../vendor/jquery-easing/jquery.easing.min.js, ../vendor/owl-carousel/owl.carousel.min.js, ../vendor/scrollreveal/scrollreveal.min.js, ../vendor/magnific-popup/jquery.magnific-popup.min.js, ../js/creative.min.js | Set-Content -Encoding:UTF8 all.min.js
+
