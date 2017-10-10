2db1f51bc72fcb4c043a693897a637a53039f177
Joao Samuel Lopes <jsnl85@gmail.com>
2017-10-10 16:57:52 +1100

    -Update: Moved src files of SP365.AddIn projects to a different repo

--- add-ins/398099f4/src/SP365.Extension.ApplicationCustomiser/src/extensions/sp365ApplicationCustomiser/loc/myStrings.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface ISp365ApplicationCustomiserApplicationCustomizerStrings {
-  Title: string;
-}
-
-declare module 'Sp365ApplicationCustomiserApplicationCustomizerStrings' {
-  const strings: ISp365ApplicationCustomiserApplicationCustomizerStrings;
-  export = strings;
-}
--- add-ins/cdcb95cd/src/SP365.AddIn.Services/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SP365.AddIn.Services.Global" Language="C#" %>
