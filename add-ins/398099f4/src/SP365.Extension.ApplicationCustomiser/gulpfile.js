8bdf995df9487679c55b1dd3658eecb5ee8d8ed0
Joao Samuel Lopes <jsnl85@gmail.com>
2017-10-10 12:26:33 +1100

    *Update: attempts to make 1 package for both the Classic and Modern UIs

--- add-ins/398099f4/src/SP365.Extension.ApplicationCustomiser/gulpfile.js
+++ add-ins/398099f4/src/SP365.Extension.ApplicationCustomiser/gulpfile.js
@@ -4,3 +4,30 @@ const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 
 build.initialize(gulp);
+
+// overrides:
+// .\node_modules\@microsoft\sp-build-core-tasks\lib\packageSolution\packageSolution\xmlGenerators\createFeatureXml.js
+//      function createFeatureXml(feature, resources) {
+//          ...
+//          Scope: feature.scope||'Web',
+//          Hidden: (feature.hidden?'TRUE':'FALSE')
+//          ...
+//      }
+// .\node_modules\@microsoft\sp-build-core-tasks\lib\packageSolution\packageSolution\xmlGenerators\createSolutionXml.js
+//      function createSolutionXml(solution, customFeatureFilenames, resources) {
+//          ...
+//          if (!solution.startPage) { properties.push({ StartPage: solution.startPage }); }
+//          if (!solution.settingsPage) { properties.push({ SettingsPage: solution.settingsPage }); }
+//          if (!solution.installedEventEndpoint) { properties.push({ InstalledEventEndpoint: solution.installedEventEndpoint }); }
+//          if (!solution.uninstallingEventEndpoint) { properties.push({ UninstallingEventEndpoint: solution.uninstallingEventEndpoint }); }
+//          if (!solution.upgradedEventEndpoint) { properties.push({ UpgradedEventEndpoint: solution.upgradedEventEndpoint }); }
+//          ...
+//      }
+
+
+// NOTE: task only to execute the sub-task called 'package-solution'
+//gulp.task('package', ['package-solution']);
+
+// ... IsClientSideSolution="false"
+// <StartPage>https://sp365.pro/add-ins/cdcb95cd?{StandardTokens}</StartPage><SettingsPage>https://sp365.pro/add-ins/cdcb95cd/settings?{StandardTokens}</SettingsPage><InstalledEventEndpoint>https://sp365-aps.azurewebsites.net/Services/AppEventReceiver.svc</InstalledEventEndpoint><UninstallingEventEndpoint>https://sp365-aps.azurewebsites.net/Services/AppEventReceiver.svc</UninstallingEventEndpoint><UpgradedEventEndpoint>https://sp365-aps.azurewebsites.net/Services/AppEventReceiver.svc</UpgradedEventEndpoint>
+// <AppPrincipal><RemoteWebApplication ClientId="*" /></AppPrincipal><AppPermissionRequests AllowAppOnlyPolicy="true" ><AppPermissionRequest Scope="http://sharepoint/content/sitecollection" Right="FullControl" /></AppPermissionRequests><RemoteEndpoints><RemoteEndpoint Url="https://sp365.pro" /><RemoteEndpoint Url="https://sp365-aps.azurewebsites.net" /><RemoteEndpoint Url="https://sp365.sopeapps.com.au" /></RemoteEndpoints>
